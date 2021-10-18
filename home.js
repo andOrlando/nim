dbe7dfa71313b34683dd4305c3cc33aba0150f67
andOrlando <bennettgillig@gmail.com>
2021-10-18 23:07:33 +0000

    Initial commit

---
+++ home.js
@@ -0,0 +1,5 @@
+// Holds UI logic and communicates with nim.js
+
+function coolio() {
+    document.getElementById("test").textContent = "heyo"
+}
---
+++ nim.js
@@ -0,0 +1,1 @@
+// We're gonna put the nim algorithm here
